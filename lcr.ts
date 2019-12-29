2b80ed8f0f0638d4d6d0d01302aba3e48b710c8f
Dylan Richardson <dylanrichardson1996@gmail.com>
2019-12-29 18:38:46 -0500

    Parse args into winner position and chip amounts

--- lcr.ts
+++ lcr.ts
@@ -1,9 +1,45 @@
-import { usolve } from 'mathjs';
+// TODO: make struct with chips, position, and isTurn
 
-const U = [
-  [1, 2],
-  [2, 1]
-];
-const b = [1, 1];
+const getParameters = () => {
+  const {
+    argv: [, , winnerStr, ...chipsStrs]
+  } = process;
 
-console.log(usolve(U, b));
+  const winner = parseInt(winnerStr);
+  const chips = chipsStrs.map(chipsStr => parseInt(chipsStr));
+
+  if (!Number.isInteger(winner)) {
+    throw new Error('Error: <winner> must be an integer.');
+  }
+
+  if (chips.length < winner) {
+    throw new Error(
+      'Error: Length of <chips>... must be greater than or equal to <winner>.'
+    );
+  }
+
+  if (!chips.every(Number.isInteger)) {
+    throw new Error('Error: <chips>... must be a list of integers.');
+  }
+
+  return { winner, chips };
+};
+
+const calculateProbability = (winner, chips) => {
+  // TODO: implement
+};
+
+const main = () => {
+  const { winner, chips } = getParameters();
+
+  const probability = calculateProbability(winner, chips);
+
+  chips.forEach((c, i) =>
+    console.log(`Position ${i + 1} starts with ${c} chip(s).`)
+  );
+  console.log(
+    `The probability of position ${winner} winning is ${probability}%.`
+  );
+};
+
+main();
