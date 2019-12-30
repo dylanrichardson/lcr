efd57147eac9832daf4c1026a4f656075e2118b7
cvalenti312@gmail.com <cvalenti312@gmail.com>
2019-12-30 01:17:29 -0500

    Add winning states. Add equational types

--- lcr.ts
+++ lcr.ts
@@ -3,6 +3,15 @@ interface GameState {
   chips: number[];
 }
 
+interface Variable {
+  state: GameState;
+  winningState: GameState;
+}
+
+type Term = number | Record<number, Variable>;
+
+type Equation = Term[];
+
 enum Die {
   Left,
   Center,
@@ -22,14 +31,14 @@ const getParameters = () => {
     argv: [, , winnerStr, ...chipsStrs]
   } = process;
 
-  const winner = parseInt(winnerStr);
+  const winner = parseInt(winnerStr) - 1;
   const chips = chipsStrs.map(chipsStr => parseInt(chipsStr));
 
   if (!Number.isInteger(winner)) {
     throw new Error('Error: <winner> must be an integer.');
   }
 
-  if (chips.length < winner) {
+  if (chips.length < winner + 1) {
     throw new Error(
       'Error: Length of <chips>... must be greater than or equal to <winner>.'
     );
@@ -42,17 +51,44 @@ const getParameters = () => {
   return { winner, chips };
 };
 
-const calculateProbability = (winner: number, chips: GameState) => {
-  // TODO: implement
+const getWinningStates = (
+  winnerPos: number,
+  numPlayers: number,
+  numChips: number
+): GameState[] => {
+  const players = new Array(numPlayers).fill(0);
+
+  return new Array(numChips).fill(0).map((_, chipIdx) => ({
+    turn: winnerPos,
+    chips: players.map((_, idx) => (idx === winnerPos ? chipIdx + 1 : 0))
+  }));
+};
+
+const getEquations = (): Equation[] => {
+  // TODO:
+  return [];
+};
+
+const solve = (eqs: Equation[]): number => {
+  // TODO:
+  return NaN;
+};
+
+const sum = (l: number[]) => l.reduce((a, b) => a + b);
+
+const calculateProbability = (winner: number, { turn, chips }: GameState) => {
+  const winningStates = getWinningStates(winner, chips.length, sum(chips));
+  console.log('winning states', winningStates);
+
+  const eqs = getEquations();
+
+  return solve(eqs);
 };
 
 const main = () => {
   const { winner, chips } = getParameters();
 
-  const probability = calculateProbability(winner, {
-    turn: 1,
-    chips
-  });
+  const probability = calculateProbability(winner, { turn: 0, chips });
 
   chips.forEach((c, i) =>
     console.log(`Position ${i + 1} starts with ${c} chip(s).`)
